2c9b310d08c08ae82d259c12b3ad22c2c38d8dec
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-06-19 00:33:47 -0600

    vercel baybee

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,39 +5,31 @@ import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 import compress from "astro-compress";
 import preact from "@astrojs/preact";
-
 import prefetch from "@astrojs/prefetch";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://atri.dad/",
   output: "hybrid",
   compressHTML: true,
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   server: {
     host: "0.0.0.0",
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3000
   },
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    compress({
-      logger: 0,
-    }),
-    preact({
-      compat: true,
-    }),
-    prefetch(),
-  ],
+  integrations: [mdx(), sitemap(), tailwind(), compress({
+    logger: 0
+  }), preact({
+    compat: true
+  }), prefetch()],
   vite: {
     ssr: {
-      noExternal: ["react-icons"],
-    },
+      noExternal: ["react-icons"]
+    }
   },
   experimental: {
-    assets: true,
-  },
+    assets: true
+  }
 });
